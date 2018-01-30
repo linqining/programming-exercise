600b37a7be3503b20e09814e40dc7976399c0c29
linqining <linqining@outlook.com>
2018-01-30 17:35:12 +0800

    1

--- 14-prime.rb
+++ 14-prime.rb
@@ -2,12 +2,22 @@
 
 def is_prime(n)
 # ....
+i=2
+j=(n/2).floor
+while (i<=j)
+  if n%i==0
+    return false
+    break
+  end
+  i+=1
+end
+return true
 end
 
 print "请输入数字 N，然后按 Enter: "
-n = gets
+n = gets.to_i
 
-if is_prime(n.to_i)
+if is_prime(n)
   puts "这是质数"
 else
   puts "这不是质数"
