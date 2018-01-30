8b030fe7700975df2268d62d00fbafc86c8433f5
linqining <linqining@outlook.com>
2018-01-30 18:25:29 +0800

    20

--- 20-sorting.rb
+++ 20-sorting.rb
@@ -3,9 +3,19 @@
 
 def filter_even(arr)
   #...
+  arr1=[]
+  i=0
+  while (i<arr.size)
+    if arr[i]%2==0
+      arr1 << arr[i]
+    end
+    i+=1
+  end
+
+  return arr1
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
+arr1=filter_even(arr).uniq.sort
 
-
-puts "________" # 应该是 [42, 46, 68, 86]
+puts "#{arr1.to_s}" # 应该是 [42, 46, 68, 86]
