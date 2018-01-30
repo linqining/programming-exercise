f1b1a8c8bf2f759dc5b7aa1000e7a10027a4fc45
linqining <linqining@outlook.com>
2018-01-30 17:56:29 +0800

    16

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -2,10 +2,18 @@
 
 def find_max(array)
   #....
+  i=1
+  t= array[0]
+  while (i<array.size)
+    if array[i]>=t
+      t=array[i]
+    end
+    i+=1
+  end
+  return t
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
 puts "Max is #{max}" # 应该是 88
-
